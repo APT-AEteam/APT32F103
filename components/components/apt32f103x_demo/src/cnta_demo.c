c90b1e4dcd4e00a6eb711426fc4a235bc0305454
lch <russell.lch@aptchip.com>
2022-09-19 09:33:41 +0800

    删除多余注释

--- components/components/apt32f103x_demo/src/cnta_demo.c
+++ components/components/apt32f103x_demo/src/cnta_demo.c
@@ -31,19 +31,16 @@ int cnta_timer_demo(void)
 {	
 	int iRet = 0;
 //  csi_pin_set_mux(PA011,PA011_CNTA_BUZ);//set counter output pin	该管脚的周期即为设置的时间
-	csi_pin_set_mux(PA05,PA05_CNTA_BUZ);  //PA05为烧录管脚，调试该管脚功能时需修改烧录口管脚
-//  csi_pin_set_mux(PB01,PB01_CNTA_BUZ);
+//	csi_pin_set_mux(PA05,PA05_CNTA_BUZ);  //PA05为烧录管脚，调试该管脚功能时需修改烧录口管脚
+  csi_pin_set_mux(PB01,PB01_CNTA_BUZ);
 //  csi_pin_set_mux(PB011,PB011_CNTA_BUZ);
 //	csi_pin_set_mux(PB012,PB012_CNTA_BUZ);
-//	csi_pin_set_mux(PA113,PA113_CNTA_REM);//set counter output pin
-//	csi_pin_set_mux(PA17,PA17_CNTA_REM);//set counter output pin,这个脚是烧录管脚的数据线，调试PA17_CNTA_REM功能，记得在测之前加个2-3秒的延时，除非是最终版本
-	
 // (byDivTemp / pclk) * (tmp_load) *1000000*2  单位us  byDivTemp只能为1，2，4，8
-	//极限时间最小:byDivTemp=1 tmp_load=1     代入得到 (1 / pclk) * (1+3) *1000000*2       
-	//极限时间最大:byDivTemp=8 tmp_load=65533 代入得到 (8 / pclk) * (65533+3) *1000000*2 
-	//当pclk确定后，如果设置的时间超出了范围，将会被设置成默认最大值
-	//当pclk为48000000hz时，极限范围为0.167-21.845us  实际我们用到的是1-21845 us
-	csi_cnta_timer_init(CNTA,1000);//1000us 进入一次中断
+//极限时间最小:byDivTemp=1 tmp_load=1     代入得到 (1 / pclk) * (1+3) *1000000*2       
+//极限时间最大:byDivTemp=8 tmp_load=65533 代入得到 (8 / pclk) * (65533+3) *1000000*2 
+//当pclk确定后，如果设置的时间超出了范围，将会被设置成默认最大值
+//当pclk为48000000hz时，极限范围为0.167-21.845us  实际我们用到的是1-21845 us
+	csi_cnta_timer_init(CNTA,16000);//1000us 进入一次中断
 	csi_cnta_start(CNTA);
 	while(1)
 	{
@@ -80,11 +77,7 @@ int cnta_pwm_demo(void)
 	csi_cnta_bt0_sync(CNTA, CNTA_PEND_CARR_SET, CNTA_MATCH_CARR_CLR,CNTA_HW_DIS);//结合bt0的定时器和pwm示例，设置硬件自动打开或者关闭载波
 	csi_cnta_start(CNTA);
  //  delay_ums(1000);
-//  CNTA->CADATAH = 0x37;
-//	CNTA->CADATAL = 0x0b;
-//	CNTA->CACON = 0x2490002;
-//	csp_cnta_soft_updata(CNTA);
-	
+
 	while(1)
 	{
 		nop;
