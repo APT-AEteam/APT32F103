969d01335ed8a50a55b796f7310a51d7e86e1d72
cqyyxb <1002260494@qq.com>
2023-01-04 09:27:55 +0800

    删除TKEY源码

--- components/csi/include/drv/tkey.h
+++ components/csi/include/drv/tkey.h
@@ -23,50 +23,37 @@ extern "C" {
 #endif
 
 
-uint32_t	wTkeyIoEnable;					 		
-uint16_t	hwTkeySenprd[17];
-uint8_t		byTkeyScanTime[17];
-uint16_t	hwTkeyTriggerLevel[17];	
-uint8_t		byPressDebounce;		
-uint8_t		byReleaseDebounce;	
-uint16_t	hwTkeyIcon[17];
-uint8_t		byMultiTimesFilter;			
-uint8_t		byValidKeyNum;				
-uint8_t 	byKeyMode;					
-uint8_t 	byBaseSpeed;				
-uint32_t 	wTkeyLongPressTime;			
-uint16_t 	hwTkeyPselMode;		
-uint16_t 	hwTkeyFVRLevel;		
-uint16_t 	hwTkeyECLevel;
-uint8_t 	byTkeyLowpowerMode;
-uint8_t 	byTkeypowerLevel;	
-uint8_t 	byTkeyWakeupLevel;	
-uint8_t		byTkeyTckdiv;
-uint8_t		byTkeyPckdiv;
-
-extern volatile uint16_t hwOffseData0Abs[17];
-extern volatile uint16_t hwOffseData1Abs[17];
-extern volatile uint16_t hwOffseData2Abs[17];
-extern volatile uint16_t hwSamplingData0[17];
-extern volatile uint16_t hwBaselineData0[17];
-extern volatile int16_t nOffsetData0[17];
-extern volatile uint16_t hwSamplingData1[17];
-extern volatile uint16_t hwBaselineData1[17];
-extern volatile int16_t nOffsetData1[17];
-extern volatile uint16_t hwSamplingData2[17];
-extern volatile uint16_t hwBaselineData2[17];
-extern volatile int16_t nOffsetData2[17];
-extern volatile uint32_t wKeyMap;
-extern volatile uint8_t byBaseUpdata;
+extern volatile uint8_t byoutflag;
+extern volatile uint8_t byScanStep;
+extern volatile uint8_t byBaseLineCnt;
+extern volatile uint32_t dwKeyMap;
+extern volatile uint16_t hwBaselineData[3][32];
+extern volatile uint16_t hwSamplingData[3][32];
+extern volatile uint8_t byBaseUpdateFlag;
+extern volatile uint8_t byTkeyTrackCnt;
+extern volatile uint32_t dwRKeyMapTemp;
+extern volatile uint32_t dwKeyMapTemp[3];
+extern volatile uint8_t byPressDebounce[3][32];
+extern volatile uint8_t byReleaseDebounce[3][32];
+extern volatile uint8_t byPostiveBuild[3][32];
+extern volatile uint8_t byNegtiveBuild[3][32];
+extern volatile uint8_t byTriggerLevel[32];
+extern volatile uint32_t dwTimeCnt;
+extern volatile uint32_t dwTkeyLongpressTime;	
+extern volatile uint8_t byTkeyNum;
+extern volatile uint8_t byMultiTimesFilter;
+extern volatile uint8_t byPressDebounceCnt;		
+extern volatile uint8_t byReleaseDebounceCnt;
+extern volatile uint8_t byKeyMode;
+extern volatile uint8_t byBaseSpeed;	
+extern volatile uint8_t byValidKeyNum;
+extern uint32_t wTkeyIoEnable;//使用TCH10 和TCH13的时候，因为默认是SWD口，库修改相应功能需要在初始化前给syscon里的修改SWD口的KEY的寄存器写入KEY；
 
 
-extern void csi_tkey_timer_handler(void);
-extern void csi_tkey_handler(void);
-extern void csi_tkey_setup_sleep(void);
-extern void csi_tkey_quit_sleep(void);
+extern void csi_tkey_sleep(void);
 extern void csi_tkey_init(void);
-extern void csi_tkey_prgm(void);
-extern void csi_tkey_parameter_init(void);
+extern void csi_key_process(void); 
+extern void csi_tkey_parameter_init(uint16_t *hwTkeySenprd,uint8_t *byTkeyIcon);
 
 #ifdef __cplusplus
 }
